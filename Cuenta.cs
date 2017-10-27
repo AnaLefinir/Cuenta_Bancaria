cd110b1421a8611c934280d27f1cacf2316607e0
Ana <analefinir@gmail.com>
2017-10-27 20:55:16 -0300

    Cree las clases Cuenta y Titulares

---
+++ Cuenta.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+public class Cuenta
+{
+    public int numeroDeCuenta;
+    public List<Titular> ListaDeTitulares = new List<Titular>();
+    public int saldo;
+}
